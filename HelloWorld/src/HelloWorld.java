185ed686b56f0318a50ce34f998806c6955a9e8a
liuemi001 <emily.ry.liu@gmail.com>
2022-04-12 21:38:09 -0500

    set up bot and added basic listener - not working yet

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,8 +2,9 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class HelloWorld {
 	
@@ -11,10 +12,20 @@ public class HelloWorld {
 
 	public static void main(String[] args) throws LoginException{
 		// TODO Auto-generated method stub
-		System.out.println(System.getProperty("java.class.path"));
-		JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
-		//jda.J.setActivity(Activity.playing("science bowl :)"));
-		//jda.setStatus(OnlineStatus.ONLINE);
+		//JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
+		JDABuilder builder = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68");
+	    
+	    // Disable parts of the cache
+	    builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+	    // Enable the bulk delete event
+	    builder.setBulkDeleteSplittingEnabled(false);
+	    // Disable compression (not recommended)
+	    builder.setCompression(Compression.NONE);
+	    // Set activity (like "playing Something")
+	    builder.setActivity(Activity.watching("TV"));
+	    
+	    builder.build();
+		
 		
 	}
 
---
+++ HelloWorld/src/MessageListener.java
@@ -0,0 +1,40 @@
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter{
+
+	public static void main(String[] args) throws LoginException{
+		// TODO Auto-generated method stub
+		JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
+	    //You can also add event listeners to the already built JDA instance
+	    // Note that some events may not be received if the listener is added after calling build()
+	    // This includes events such as the ReadyEvent
+	    jda.addEventListener(new MessageListener());
+
+	}
+	
+	@Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContentDisplay());
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("hi i'm here!");
+        }
+    }
+
+}
