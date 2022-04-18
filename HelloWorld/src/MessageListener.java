b8515451e17cd9b4dc97974335076f0bedd6da83
liuemi001 <emily.ry.liu@gmail.com>
2022-04-18 14:03:45 -0500

    added basic event listener

--- HelloWorld/src/MessageListener.java
+++ HelloWorld/src/MessageListener.java
@@ -9,32 +9,22 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MessageListener extends ListenerAdapter{
 
-	public static void main(String[] args) throws LoginException{
+	//public static void main(String[] args) throws LoginException{
 		// TODO Auto-generated method stub
-		JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
+		//JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
 	    //You can also add event listeners to the already built JDA instance
 	    // Note that some events may not be received if the listener is added after calling build()
 	    // This includes events such as the ReadyEvent
-	    jda.addEventListener(new MessageListener());
+	   // jda.addEventListener(new MessageListener());
 
-	}
+//	}
 	
 	@Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if (event.isFromType(ChannelType.PRIVATE))
-        {
-            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-                                    event.getMessage().getContentDisplay());
-        }
-        else
-        {
-            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
-                        event.getMessage().getContentDisplay());
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("hi i'm here!");
-        }
+		System.out.println(event.getChannel());
+		System.out.println(event.getMessage());
+	    event.getChannel().sendMessage("hello!").queue();
     }
 
 }
