9c91b883a671dd6d26fbea30513dd2352b0a1f90
Euphoria13 <kevineyang271@gmail.com>
2022-04-17 22:01:23 -0500

    edited sendGet

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -12,13 +12,21 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class HelloWorld {
 	
-	public static JDA jda; 
+	public static JDA jda;
+
+	private final HttpClient httpClient = HttpClient.newBuilder()
+			.version(HttpClient.Version.HTTP_2)
+			.build();
+
 
 	public static void main(String[] args) throws LoginException{
 		// TODO Auto-generated method stub
 		//JDA jda = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68").build();
 		JDABuilder builder = JDABuilder.createDefault("OTU0ODE0MzAzNTcwMTAwMjg0.YjYmHQ.BzkEwALqpo9cvD9YbfjFhR5RA68");
-	    
+
+		Java11HttpClientExample obj = new Java11HttpClientExample();
+		JSONObject QuestionFile=obj.sendGet();
+
 	    // Disable parts of the cache
 	    builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
 	    // Enable the bulk delete event
@@ -32,23 +40,22 @@ public class HelloWorld {
 		
 		
 	}
-	
-	private void sendGet() throws Exception {
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://scibowldb.com/api/questions/random%22"))
-                .setHeader("User-Agent", "Java 11 HttpClient Bot")
-                .build();
+	//Method that returns a JSONObject with all of the information for a single question
+	//To get a specific thing from the JSON file, call (JSONObject Name).get("(Name of thing ie. question)");
+	private JSONObject sendGet() throws Exception {
+		HttpRequest request = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://scibowldb.com/api/questions/random"))
+				.setHeader("User-Agent", "Java 11 HttpClient Bot")
+				.build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // print status code
-        System.out.println(response.statusCode());
-
-        // print response body
-        System.out.println(response.body());
-
+		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+		String Edited= response.body().substring(12,response.body().length()-2);
+		System.out.println(Edited);
+		JSONParser parser = new JSONParser();
+		return (JSONObject) parser.parse(Edited);
     }
 
 }
