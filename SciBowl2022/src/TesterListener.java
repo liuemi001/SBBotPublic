db63cae7a90f50d17dc0a76b87e6b5b0d1a84bd1
Euphoria13 <kevineyang271@gmail.com>
2022-04-23 21:29:04 -0500

    Test to add jdk

---
+++ SciBowl2022/src/TesterListener.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class TesterListener extends ListenerAdapter{
+	public String prefix = "!";
+	
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event)
+	{
+		String[] args = event.getMessage().getContentRaw().split(" ");
+		//Thread newThread = new Thread(() -> {
+			if (args[0].equalsIgnoreCase(prefix + "hi")) {
+				
+				//DatabaseUnpacking unpack = new DatabaseUnpacking();
+				//unpack.helloint = 2;
+				//try {
+				//	TimeUnit.SECONDS.sleep(3);
+				//} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+				//	e.printStackTrace();
+				//}
+				event.getMessage().reply("listener 2 here").queue();
+			}
+		//});
+		//newThread.start();
+		
+	}
+	
+	
+}
